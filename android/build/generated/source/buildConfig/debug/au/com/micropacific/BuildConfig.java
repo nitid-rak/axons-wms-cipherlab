4d5e08ef2c7657b3e381516d1f24c93d41d937e3
Rakfaeng <nitid.rf@gmail.com>
2023-06-27 10:15:12 +0700

    fix library cipherlab

---
+++ android/build/generated/source/buildConfig/debug/au/com/micropacific/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package au.com.micropacific;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "au.com.micropacific";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ android/build/generated/source/buildConfig/release/au/com/micropacific/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package au.com.micropacific;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "au.com.micropacific";
+  public static final String BUILD_TYPE = "release";
+}
